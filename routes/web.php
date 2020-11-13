3a3842a56d44a3e281cabaf6fb886e0cf84f3ddb
matador19 <71817060+matador19@users.noreply.github.com>
2020-11-13 17:59:19 +0300

    Version 1.1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/','PagesController@index');
+Route::get('/login','PagesController@login');
+Route::get('/register','PagesController@register');
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
